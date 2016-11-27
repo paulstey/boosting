f5479d1bf6d9408fcde1e95fd551afd9ecdd39a2
paulstey <paulstey@gmail.com>
2016-11-26 23:00:33 -0500

    first commit

---
+++ src/measures.jl
@@ -0,0 +1,68 @@
+# measures.jl
+function _hist_add!{T}(counts::Dict{T,Int}, labels::Vector{T}, region::Range1{Int})
+    for i in region
+        lbl = labels[i]
+        counts[lbl] = get(counts, lbl, 0) + 1
+    end
+    return counts
+end
+
+
+function _hist_sub!{T}(counts::Dict{T,Int}, labels::Vector{T}, region::Range1{Int})
+    for i in region
+        lbl = labels[i]
+        counts[lbl] -= 1
+    end
+    return counts
+end
+
+
+function _hist_shift!{T}(counts_from::Dict{T,Int}, counts_to::Dict{T,Int}, labels::Vector{T}, region::Range1{Int})
+    for i in region
+        lbl = labels[i]
+        counts_from[lbl] -= 1
+        counts_to[lbl] = get(counts_to, lbl, 0) + 1
+    end
+end
+
+
+_hist{T}(labels::Vector{T}, region::Range1{Int} = 1:endof(labels)) = _hist_add!(Dict{T,Int}(), labels, region)
+
+
+function _info_gain{T}(N1::Int, counts1::Dict{T,Int}, N2::Int, counts2::Dict{T,Int})
+    N = N1 + N2
+    H = - N1/N * _set_entropy(counts1, N1) - N2/N * _set_entropy(counts2, N2)
+    return H
+end
+
+
+function _neg_z1_loss{T<:Real}(labels::Vector, weights::Vector{T})
+    missmatches = labels .!= majority_vote(labels)
+    loss = sum(weights[missmatches])
+    return -loss
+end
+
+
+function _weighted_error{T<:Real}(actual::Vector, predicted::Vector, weights::Vector{T})
+    mismatches = actual .!= predicted
+    err = sum(weights[mismatches]) / sum(weights)
+    return err
+end
+
+
+function majority_vote(labels::Vector)
+    if length(labels) == 0
+        return nothing
+    end
+    counts = _hist(labels)
+    top_vote = labels[1]
+    top_count = -1
+    for (k,v) in counts
+        if v > top_count
+            top_vote = k
+            top_count = v
+        end
+    end
+    return top_vote
+end
+
---
+++ src/testing.jl
@@ -0,0 +1,25 @@
+
+using Distributions
+
+
+n = 200
+p = 10
+X = hcat(ones(n), randn(n, p-1))
+
+noise_coefs = zeros(p - 5)
+β = vcat([1.5, 1.1, -10.5, 4.5, 1.9], noise_coefs)
+η = X*β                                                   # linear predictor
+pr = 1./(1 + exp(-η))                                     # inv-logit
+
+# simulate outcome variable
+y = map(π -> rand(Binomial(1, π)), pr)                    
+mean(y)
+
+
+model, coeffs = build_adaboost_stumps(y, X, 7);
+# apply learned model
+apply_adaboost_stumps(model, coeffs, randn(p))
+# get the probability of each label
+apply_adaboost_stumps_proba(model, coeffs, [5.9,3.0,5.1,1.9], ["setosa", "versicolor", "virginica"])
+# run n-fold cross validation for boosted stumps, using 7 iterations and 3 folds
+accuracy = nfoldCV_stumps(y, X, 7, 3)
