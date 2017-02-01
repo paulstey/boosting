e3c65a538a3b50a5da3c2a9c9a638454924779f5
paulstey <paulstey@gmail.com>
2017-02-01 10:25:08 -0500

    roc_auc_score() seems to be working

--- src/auc.jl
+++ src/auc.jl
@@ -1,42 +1,7 @@
 
-function trapezoidal_area(x1, x2, y1, y2)
-    base = abs(x1 - x2)
-    height = (y1 + y2)/2
-    soln = base * height
-    soln
-end
-
-
-function auc(y, f, n_pos, n_neg)
-    idx_order = sortperm(f, rev = true)
-    y_sorted = y[idx_order]
-    fp = 0
-    tp = 0
-    fp_prev = 0
-    tp_prev = 0
-    area = 0.0
-
-
-    for i = 1:length(y_sorted)
-        if i > 1 && f[i] ≠ f[i-1]
-            area += trapezoidal_area(fp, fp_prev, tp, tp_prev)
-            fp_prev = fp
-            tp_prev = tp
-        end
-        if y_sorted[i] == 1
-            tp += 1
-        else
-            fp += 1
-        end
-    end
-    area += trapezoidal_area(n_neg, fp_prev, n_neg, tp_prev)
-    area = area/(n_pos * n_neg)
-    return area
-end
-
-
-
-
+# This set of functions is based entirely on the scikit-learn package's
+# method of calculating an ROC's area under the curve (AUC).
+# The functions below produce the same output as scikit-learn.
 
 
 
@@ -55,14 +20,10 @@ function _binary_clf_curve(y_true, y_score)
     return (fps, tps, y_score[threshold_idxs])
 end
 
-
-
-y = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1]
-y_score = [0.3, 0.2, 0.3, 0.23, 0.5, 0.34, 0.45, 0.54, 0.6, 0.7, 0.8, 0.65, 0.5, 0.4, 0.3, 0.2, 0.6, 0.7, 0.5]
-
-_binary_clf_curve(y, y_score)
-
-
+# y = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1]
+# y_score = [0.3, 0.2, 0.3, 0.23, 0.5, 0.34, 0.45, 0.54, 0.6, 0.7, 0.8, 0.65, 0.5, 0.4, 0.3, 0.2, 0.6, 0.7, 0.5, 0.2, 0.1, 0.7, 0.2, 0.7, 0.4]
+#
+# _binary_clf_curve(y, y_score)
 
 
 function roc_curve(y_true, y_score)
@@ -72,19 +33,14 @@ function roc_curve(y_true, y_score)
     return (fpr, tpr, thresholds)
 end
 
-roc_curve(y, y_score)
-
-
-
-
-
+# roc_curve(y, y_score)
 
 
 
 
 """
 Given 2 vectors, `x` and `y`, this function returns the indices that
-sort the elements by `x`, with `y` breaking ties. See the example below. 
+sort the elements by `x`, with `y` breaking ties. See the example below.
 
 julia> a = [2, 1, 3, 2]
 julia> b = [3, 4, 1, 0]
@@ -134,57 +90,53 @@ function sortperm2(x, y; rev = false)
     res
 end
 
-
-x1 = [4, 1, 1, 2, 3, 3]
-y1 = [2, 3, 2, 5, 4, 3]
-
-sortperm2(x1, y1)
-a = [1, 5, 1, 4, 3, 4, 4]
-b = [9, 4, 0, 4, 0, 2, 1]
-
-ord = sortperm2(a, b, rev = true)
-hcat(a[ord], b[ord])
-
-
-a = [1, 5, 1, 4, 3, 4, 4, 3, 1, 4, 5, 3, 5]
-b = [9, 4, 0, 4, 0, 2, 1, 2, 1, 3, 2, 1, 1]
-
-ord = sortperm2(a, b)
-hcat(a[ord], b[ord])
-
-
-
-#
-# function sortperm2(x, y; rev = false)
-#     mat = hcat(x, y)
-#     n = size(mat, 2)
-#     ord = sortperm(mat[:, 1], rev)
-#
-#     for i = 1:(n-1)
-#         if mat[i, 1] == mat[i, 2]
-#
-#
-# end
-#
-#
-# x1 = [4, 1, 1, 2, 3, 3]
-# y1 = [2, 3, 2, 5, 4, 3]
-#
-# sortperm2(x1, y1)
-# a = [1, 5, 1, 4, 3, 4, 4]
-# b = [9, 4, 0, 4, 0, 2, 1]
+# a = [1, 5, 1, 4, 3, 4, 4, 3, 1, 4, 5, 3, 5]
+# b = [9, 4, 0, 4, 0, 2, 1, 2, 1, 3, 2, 1, 1]
 #
 # ord = sortperm2(a, b, rev = true)
 # hcat(a[ord], b[ord])
-
-
-
-
-
-
 #
-# function auc(x, y, reorder = false)
-#     direction = 1
-#     if reorder
-#         order = sortperm2(x, y)
-# end
+# ord = sortperm2(a, b)
+# hcat(a[ord], b[ord])
+
+
+function trapsum(y, x)
+    d = diff(x)
+    res = sum((d .* (y[2:end] + y[1:(end-1)]) ./ 2.0))
+    res
+end
+
+# trapsum([1, 2, 3], [4, 6, 8])     # 8.0
+
+
+function auc(x, y, reorder = false)
+    direction = 1
+    if reorder
+        order = sortperm2(x, y)
+        x, y = x[order], y[order]
+    else
+        dx = diff(x)
+        if any(dx .<= 0)
+            if all(dx .<= 0)
+                direction = -1
+            else
+                error("Reordering is not turned on, and the x array is not increasing: $x")
+            end
+        end
+    end
+    area = direction * trapsum(y, x)
+    return area
+end
+
+
+function roc_auc_score(y_true, y_score)
+    if length(Set(y_true)) == 1
+        error("Only one class present in y_true. ROC AUC score is not defined in that case.")
+    end
+
+    fpr, tpr, thresholds = roc_curve(y_true, y_score)
+    res = auc(fpr, tpr, true)
+    return res
+end
+
+# roc_auc_score(y, y_score)
